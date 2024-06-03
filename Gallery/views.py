bde2696138082fbd8edbcc30e4c4a1ec7121b983
E0ho <leeyeongho3404@gmail.com>
2024-06-03 23:26:00 +0900

    fix: 메인 페이지 이미지 출력 오류 해결

--- Gallery/views.py
+++ Gallery/views.py
@@ -11,16 +11,17 @@ from GalleryUser.models import User
 def gallery_list(request):
     gallery_list = Picture.objects.all()
     context = {'gallery_li': gallery_list}
+
     return render(request, 'gallery/gallery.html', context)
 
 
 def detail(requests, picture_id):
     if picture_id is not None:
         picture = get_object_or_404(Picture, picture_id=picture_id)
-        image_data = base64.b64encode(picture.image).decode()  # <-- 이 부분 해결하면 될 것 같음
-        image_data = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
+        # image_data = base64.b64encode(picture.image).decode()  # <-- 이 부분 해결하면 될 것 같음
+        # image_data = 'iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='
         return render(requests, "detail.html",
-                      {"picture": picture, 'img': image_data})
+                      {"picture": picture})      # , 'img': image_data
 
 
 def create_comment(requests, picture_id):
@@ -57,16 +58,19 @@ def new_register(request):
 
 
 def create(request):
+    print()
     picture = Picture(
         title=request.POST.get('title'),
-        image=request.POST.get('image'),
+        image=request.FILES.get('image'),
         content=request.POST.get('content'),
         created_at=request.POST.get('created_at'),
-        user_id=request.POST.get('user_id')
+        user_id=request.user
     )
+    # print(image)
     picture.save()
 
     return redirect('gallery:index')
+    # return HttpResponse(print(request.POST))
 
 
 def search(request):
